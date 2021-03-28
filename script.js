f53031453c00e4c6b4fbf8f485946cdbabd0263c
Arlen Freitas <arlenfreitas1@gmail.com>
2021-03-28 13:37:45 -0300

    checar campos

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const btnEntrar = document.getElementById('button-login');
 const personalizado = document.getElementById('Personalizado');
 const labelGender = document.getElementById('generos');
 const genderCustom = document.createElement('input');
-
+const botao = document.getElementById('facebook-register');
 const createGenderCustom = () => labelGender.appendChild(genderCustom);
 
 function alerta() {
@@ -12,3 +12,24 @@ function alerta() {
 
 btnEntrar.addEventListener('click', alerta);
 personalizado.addEventListener('click', createGenderCustom);
+
+function checarCampo() {
+  const nome = document.getElementById('nomeLogin').value;
+  const sobrenome = document.getElementById('sobrenomeLogin').value;
+  const telefone = document.getElementById('telefoneLogin').value;
+  const nascimento = document.getElementById('nascimentoLogin').value;
+  const senha = document.getElementById('senhaLogin').value;
+  const elementos = [nome, sobrenome, telefone, nascimento, senha];
+  for (let index = 0; index < elementos.length; index += 1) {
+    if (elementos[index] === '') {
+      campo.style.display = 'flex';
+      campo.innerText = 'Campos inválidos';
+      break;
+    }
+  }
+  if (campo.innerText !== 'Campos inválidos') {
+    userLoged(elementos);
+  }
+}
+
+botao.addEventListener('click', checarCampo);
