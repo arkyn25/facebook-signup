843edfc628d23a6b966b70de5c1c4315248671d8
renatogsouza <renatomacedo1995@gmail.com>
2021-04-02 17:12:55 -0300

    gender-custom

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const personalizado = document.getElementById('Personalizado');
 const labelGender = document.getElementById('generos');
 const genderCustom = document.createElement('input');
 
-const createGenderCustom = () => labelGender.appendChild(genderCustom);
+const createGenderCustom = () => {
+  labelGender.appendChild(genderCustom);
+  genderCustom.name = 'gender-custom';
+  genderCustom.placeholder = 'Gênero (opcional)';
+};
 
 function alerta() {
   alert(`${document.getElementById('user-email-phone').value}`);
